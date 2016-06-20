24f54291c67b47db78cc39d26eeb14a918c23538
Shubham Jain <bitloks@gmail.com>
2016-06-21 04:23:26 +0530

    Solved  CountingSort-3.

--- Sorting/Sorting/CountingSort-3.py
+++ Sorting/Sorting/CountingSort-3.py
@@ -1,7 +1,10 @@
+n = int(input())
+a = []
+for i in range(0,n):
+    data=input().strip().split(' ')
+    a.append(int(data[0]))
 
-        
-m = int(input().strip())
-a = [int(x) for x in input().split()] #to input list
+
 listofcounting = [0] * 100 #initializing a list of size 100 with zeroes
 for i in range(0,100):
     x=a.count(i) #finding how many times the number apperas in the list
@@ -14,5 +17,10 @@ for i in range(0,100):
     if (listofcounting[i]):
         sum+=listofcounting[i]
         a.append(sum)
-print(" ".join(map(str,a)))
+    if sum==a[i]:
+        a.append(sum)
+print(" ".join(map(str,a[1:])))
 
+
+
+    
